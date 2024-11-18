4c7597464b0df50467a3601c1219e276c0d07fae
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-19 04:13:02 +0900

    feat: 세션 및 accessToken 저장 기능 추가

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -12,16 +12,18 @@ export const useAuth = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshToken');
     localStorage.removeItem('username');
+    localStorage.removeItem('session');
     setUsername(null);
   };
 
   const handleCallback = async (code) => {
     try {
       const backendResponse = await axios.get(`${import.meta.env.VITE_DEFAULT_API_URI}/login?code=${code}`);
-      const { accessToken, refreshToken, username } = backendResponse.data;
+      const { accessToken, refreshToken, username, session } = backendResponse.data;
 
+      localStorage.setItem('accessToken', accessToken);
       localStorage.setItem('refreshToken', refreshToken);
-      localStorage.setItem('username', username);
+      localStorage.setItem('session', session);
 
       setUsername(username);
     } catch (error) {
