8624224f35c4206d6b8559ac2bb19ba33bfbe4e7
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-19 01:53:21 +0900

    chore: 의존성 및 config 재설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
     plugins: [react()],
     server: {
-        port : 3000
-    }
-})
+        port: 3000
+    },
+    define: {
+        'process.env': process.env,
+    },
+});
