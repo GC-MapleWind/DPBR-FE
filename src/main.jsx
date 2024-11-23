ddd819e1b245e09bd21c2bc0d8fbf622b0271673
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-23 14:16:29 +0900

    refactor: 코드 정리

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {createRoot} from 'react-dom/client';
-import {BrowserRouter, Routes, Route, useSearchParams} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {GoogleOAuthProvider} from '@react-oauth/google';
 import App from './components/App';
 import HomePage from './pages/HomePage';
-import {useAuth} from './hooks/useAuth';
 import './style.css';
-
-const Callback = () => {
-    const [searchParams] = useSearchParams();
-    const code = searchParams.get('code');
-    const {handleCallback} = useAuth();
-
-    useEffect(() => {
-        if (code) {
-            handleCallback(code);
-        }
-    }, [code, handleCallback]);
-
-    return <div>로그인 중...</div>;
-};
 
 function Main() {
     return (
@@ -28,15 +13,8 @@ function Main() {
                 <Route path="/" element={<App/>}>
                     <Route index element={<HomePage/>}/>
                     <Route path="ranking" element={<div>랭킹 페이지</div>}/>
-                    <Route path="statistics" element={<div>통계 페이지</div>}/>
-                    <Route path="profile" element={<div>내 정보 페이지</div>}/>
-                    <Route path="callback" element={<Callback/>}/>
                 </Route>
             </Routes>
-            <div>main</div>
-            <footer className="footer">
-                <img src="../public/footer.svg" alt="Footer Image"/>
-            </footer>
         </BrowserRouter>
     );
 }
