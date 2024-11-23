07f50a2a95cc8fbc4ae40722f5f104e66aa722c5
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-11-23 14:14:58 +0900

    refactor: footer와 NavBar가 모든 페이지에서 보이도록 코드 이동

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Outlet} from 'react-router-dom';
 import NavBar from './NavBar';
 import '../styles/App.css';
+import '../style.css';
 
 function App() {
     return (
-        <div>
+        <div className="app-container">
             <NavBar/>
+            <main>
+                <Outlet/>
+            </main>
+            <footer className="footer">
+                <img src="/footer.svg" alt="Footer Image"/>
+            </footer>
         </div>
     );
 }
